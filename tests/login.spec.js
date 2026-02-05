c434e0d520c5a123024d2c1cfa973ac6da0fbbc8
Jonathan Malagon <jomalagon@apexsystems.com>
2026-02-05 13:12:53 -0600

    Fixtures-1: add Fixtures and docs

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,7 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/InternalPortal';
+import { test, expect } from '../fixtures/fixtures';
 
-test('login', async ({ page }) => {
-
-    const Login = new LoginPage(page)
-
-    await Login.gotoLoginPage();
-    await Login.login('013309', 'secrete');
+test('login', async ({ loginPage }) => {
+    await loginPage.login('013309', 'secrete');
 
 //   await page.goto('https://myapexinternal.apexsystemsinc.com/psp/INTERNAL/?cmd=login&languageCd=ENG&');
 //   await page.locator('#userid').click();
